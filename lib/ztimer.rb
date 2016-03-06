36cd05d5f2cd4aaedfe44fe96b2f97de3dea032a
Groza Sergiu <serioja90@gmail.com>
2016-03-06 14:11:51 +0100

    NEW: iproved timer performance

--- lib/ztimer.rb
+++ lib/ztimer.rb
@@ -1,22 +1,24 @@
 require 'set'
 require 'hitimes'
-require 'thread_safe'
+require 'pqueue'
+require 'lounger'
+
 require "ztimer/version"
-require "ztimer/sorted_store"
 require "ztimer/slot"
+require "ztimer/sorted_store"
+require "ztimer/watcher"
 
 module Ztimer
   @concurrency  = 20
-  @slots        = Ztimer::SortedStore.new
+  @watcher      = Ztimer::Watcher.new(){|slot| execute(slot) }
   @metric       = Hitimes::Metric.new("Notifier")
-  @monitor      = nil
-  @running      = 0
   @workers_lock = Mutex.new
-  @io_lock      = Mutex.new
   @queue        = Queue.new
+  @running      = 0
+  @count        = 0
 
   class << self
-    attr_reader :concurrency, :running
+    attr_reader :concurrency, :running, :count
 
     def after(milliseconds, &callback)
       enqueued_at = @metric.utc_microseconds
@@ -29,7 +31,7 @@ module Ztimer
     end
 
     def jobs_count
-      return @slots.count
+      return @watcher.jobs
     end
 
     def concurrency=(new_value)
@@ -40,58 +42,26 @@ module Ztimer
     protected
 
     def add(slot)
-      @io_lock.synchronize do
-        @slots << slot
-        restart_monitor if @slots.first == slot || @monitor.nil? || !@monitor.alive?
-      end
+      @count += 1
+      @watcher << slot
     end
 
-    def restart_monitor
-      if @monitor && @monitor.status == "sleep"
-        @monitor.kill
-      else
-        return if @monitor
-      end
-
-      @monitor = Thread.new do
-        loop do
-          delay = @io_lock.synchronize { @slots.empty? ? nil : @slots.first.expires_at - @metric.utc_microseconds }
-          break if delay.nil?
-
-          select(nil, nil, nil, delay / 1_000_000.to_f) if delay > 1 # 1 microsecond of cranularity
-
-          @io_lock.synchronize do
-            while slot = get_first_expired do
-              slot.started_at = @metric.utc_microseconds
-              execute(slot)
-            end
-          end
-        end
-      end
-      @monitor.abort_on_exception = true
-    end
-
-    def get_first_expired
-      slot = @slots.first
-      if slot && (slot.expires_at < @metric.utc_microseconds)
-        @slots.shift
-      end
-
-      return slot
-    end
 
     def execute(slot)
-      @queue.push slot
+      @queue << slot
 
       @workers_lock.synchronize do
         [@concurrency - @running, @queue.size].min.times do
           @running += 1
           worker = Thread.new do
             begin
-              while !@queue.empty? && (slot = @queue.pop(true))
+              while !@queue.empty? && @queue.pop(true) do
                 slot.executed_at = @metric.utc_microseconds
                 slot.callback.call(slot) unless slot.callback.nil?
               end
+            rescue ThreadError
+              # queue is empty
+              puts "queue is empty"
             rescue => e
               STDERR.puts e.inspect + (e.backtrace ? "\n" + e.backtrace.join("\n") : "")
             end
