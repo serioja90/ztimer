b0d301efec0037551791c3dd16b8476f5c3b7f75
Groza Sergiu <groza@opinioni.net>
2022-04-27 18:38:03 +0200

    NEW: implemented `.at()`, `.secondly()`, `.minutely()`, `.hourly()`, `.daily()` and `.day_of_week()` methods + added `:start_at` param to `.every()` method

--- lib/ztimer.rb
+++ lib/ztimer.rb
@@ -1,8 +1,11 @@
-require "ztimer/version"
-require "ztimer/slot"
-require "ztimer/sorted_store"
-require "ztimer/watcher"
+# frozen_string_literal: true
 
+require 'ztimer/version'
+require 'ztimer/slot'
+require 'ztimer/sorted_store'
+require 'ztimer/watcher'
+
+# Implements a timer which allows to execute a block with a delay, recurrently or asynchronously.
 class Ztimer
   @default_instance = nil
 
@@ -10,7 +13,7 @@ class Ztimer
 
   def initialize(concurrency: 20)
     @concurrency  = concurrency
-    @watcher      = Ztimer::Watcher.new(){|slot| execute(slot) }
+    @watcher      = Ztimer::Watcher.new { |slot| execute(slot) }
     @workers_lock = Mutex.new
     @count_lock   = Mutex.new
     @queue        = Queue.new
@@ -18,6 +21,7 @@ class Ztimer
     @count        = 0
   end
 
+  # Execute the code block asyncrhonously right now
   def async(&callback)
     enqueued_at = utc_microseconds
     slot        = Slot.new(enqueued_at, enqueued_at, -1, &callback)
@@ -25,9 +29,10 @@ class Ztimer
     incr_counter!
     execute(slot)
 
-    return slot
+    slot
   end
 
+  # Execute the code block after the specified delay
   def after(milliseconds, &callback)
     enqueued_at = utc_microseconds
     expires_at  = enqueued_at + milliseconds * 1000
@@ -35,38 +40,103 @@ class Ztimer
 
     add(slot)
 
-    return slot
+    slot
   end
 
-  def every(milliseconds, &callback)
-    enqueued_at = utc_microseconds
-    expires_at  = enqueued_at + milliseconds * 1000
-    slot        = Slot.new(enqueued_at, expires_at, milliseconds * 1000, &callback)
+  # Execute the code block at a specific datetime
+  def at(datetime, &callback)
+    enqueued_at = datetime.to_f * 1_000_000
+
+    slot = Slot.new(enqueued_at, enqueued_at, -1, &callback)
+    add(slot)
+
+    slot
+  end
+
+  # Execute the code block every N milliseconds.
+  # When :start_at is specified, the first execution will start at specified date/time
+  def every(milliseconds, start_at: nil, &callback)
+    enqueued_at = start_at ? start_at.to_f * 1_000_000 : utc_microseconds
+    expires_at = enqueued_at + milliseconds * 1000
+    slot = Slot.new(enqueued_at, expires_at, milliseconds * 1000, &callback)
 
     add(slot)
 
-    return slot
+    slot
+  end
+
+  # Run ztimer every N seconds, starting with the nearest time slot (ex. secondly(5)
+  # will run at second 0, 5, 10, 15, etc.)
+  def secondly(seconds, offset: 0, &callback)
+    start_time = utc_microseconds
+    milliseconds = (seconds.to_f * 1000).to_i
+    enqueued_at = start_time - (start_time % (milliseconds * 1000)) + offset * 1_000_000
+    expires_at = enqueued_at + milliseconds * 1000
+
+    slot = Slot.new(enqueued_at, expires_at, milliseconds * 1000, &callback)
+    add(slot)
+
+    slot
+  end
+
+  # Run ztimer every N minutes, starting at the nearest time slot (ex. minutely(2) will run at minute 0, 2, 4, 6, etc.)
+  def minutely(minutes, offset: 0, &callback)
+    secondly(minutes.to_f * 60, offset: offset.to_f * 60, &callback)
+  end
+
+  # Run ztimer every N hours, starting at the nearest time slot (ex. hourly(2) will run at hour 0, 2, 4, 6, etc.)
+  def hourly(hours, offset: 0, &callback)
+    minutely(hours.to_f * 60, offset: offset.to_f * 60, &callback)
+  end
+
+  def daily(days, offset: 0, &callback)
+    raise ArgumentError, "Days number should be > 0: #{days.inspect}" if days.to_f <= 0
+
+    hourly(days.to_f * 24, offset: offset.to_f * 24, &callback)
+  end
+
+  def day_of_week(day, &callback)
+    days = %w[sun mon tue thu wen fri sat]
+    current_day = Time.now.wday
+
+    index = day.to_i
+    if day.is_a?(String)
+      # Find day number by day name
+      index = days.index { |day_name| day.strip.downcase == day_name }
+      raise ArgumentError, "Invalid week day: #{day.inspect}" if index.nil?
+    elsif index.negative? || index > 6
+      raise ArgumentError, "Invalid week day: #{day.inspect}"
+    end
+
+    offset = 0
+    offset = (current_day > index ? index - current_day : current_day - index) if current_day != index
+
+    daily(7, offset: offset, &callback)
+  end
+
+  def days_of_week(*args, &callback)
+    args.map { |day| day_of_week(day, &callback) }
   end
 
   def jobs_count
-    return @watcher.jobs
+    @watcher.jobs
   end
 
   def concurrency=(new_value)
-    raise ArgumentError.new("Invalid concurrency value: #{new_value}") unless new_value.is_a?(Fixnum) && new_value >= 1
+    value_is_integer = new_value.is_a?(Integer)
+    raise ArgumentError, "Invalid concurrency value: #{new_value}" unless value_is_integer && new_value >= 1
+
     @concurrency = new_value
   end
 
-
   def stats
     {
-      running:   @running,
+      running: @running,
       scheduled: @watcher.jobs,
       executing: @queue.size,
-      total:     @count
+      total: @count
     }
   end
-
 
   def self.method_missing(name, *args, &block)
     @default_instance ||= Ztimer.new(concurrency: 20)
@@ -108,12 +178,13 @@ class Ztimer
           begin
             current_slot.executed_at = utc_microseconds
             current_slot.callback.call(current_slot) unless current_slot.callback.nil? || current_slot.canceled?
-          rescue => e
-            STDERR.puts e.inspect + (e.backtrace ? "\n" + e.backtrace.join("\n") : "")
+          rescue StandardError => e
+            backtrace = e.backtrace ? "\n#{e.backtrace.join("\n")}" : ''
+            warn e.inspect + backtrace
           end
         end
       rescue ThreadError
-        puts "queue is empty"
+        puts 'queue is empty'
       end
       @workers_lock.synchronize { @running -= 1 }
     end
@@ -121,6 +192,6 @@ class Ztimer
   end
 
   def utc_microseconds
-    return Time.now.to_f * 1_000_000
+    Time.now.to_f * 1_000_000
   end
 end
